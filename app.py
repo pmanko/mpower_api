086326dabccc69296047d817ec26d145b030a84f
Piotr Mankowski <piotr.mankowski@gmail.com>
2017-11-06 21:52:16 -0500

    Initialized project skeleton, although  does not work yet

---
+++ app.py
@@ -0,0 +1,29 @@
+
+#!flask/bin/python
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+patients = [
+    {
+        'id': 1,
+        'first_name': 'Piotr',
+        'last_name': 'Mankowski'
+    },
+    {
+        'id': 2,
+        'first_name': 'Bob',
+        'last_name': 'Dole'
+    }
+]
+
+
+@app.route('/')
+def hello():
+    return 'Welcome to mPOWEr!'
+
+@app.route('/api/v1.0/patients', methods=['GET'])
+def get_tasks():
+    return jsonify({'patients': patients})
+
+if __name__ == '__main__':
+    app.run(debug=True)
