ae8380800e5137705e4fe597a520ff9ebfaa1cd9
Piotr Mankowski <pmanko@uw.edu>
2017-11-29 06:42:39 -1000

    Continuing setup work for #2

---
+++ app/project/models.py
@@ -0,0 +1,13 @@
+from project import db, app
+
+class Clinic(db.Model)
+    __tablename__ = "clinics"
+
+class Site(db.Model)
+    __tablename__ = "sites"
+
+class Patient(db.Model)
+    __tablename__ = "patients"
+
+class User(db.Model)
+    __tablename__ = "users"
